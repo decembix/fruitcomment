cbaba37fe286308175804f69abdf6a2ea2d5edb7
decembix <xyzysh@gmail.com>
2025-02-05 01:49:20 +0900

    pyc gitignore

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import home, search_tv_shows
+from .views import home, search_tv_shows, movie, drama
 
 
 urlpatterns = [
     path('', home, name='home'),  # 기본 페이지
     path('search/', search_tv_shows, name='search_tv_shows'),
+    path('movie/', movie, name='movie'),
+    path('drama/', drama, name='drama'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,14 +4,14 @@ from django.http import JsonResponse
 from django.conf import settings
 
 # TMDB API 키 (보안을 위해 실제 프로젝트에서는 .env 파일 사용 추천)
-TMDB_API_KEY = "YOUR_TMDB_API_KEY"
+TMDB_API_KEY = "186af4143351e664bf9c070b58990da1"
 
 def search_tv_shows(request):
     query = request.GET.get('query', '')  # 사용자가 검색한 키워드 가져오기
     if not query:
         return JsonResponse({'error': '검색어를 입력하세요.'}, status=400)
 
-    url = f"https://api.themoviedb.org/3/search/tv?api_key=186af4143351e664bf9c070b58990da1&query={query}&language=ko-KR"
+    url = f"https://api.themoviedb.org/3/search/tv?api_key={TMDB_API_KEY}&query={query}&language=ko-KR"
     response = requests.get(url)
     data = response.json()
 
@@ -22,3 +22,9 @@ def home(request):
 '''
 def home(request):
     return render(request, 'index.html')
+
+def movie(request):
+    return render(request, 'movie.html')
+
+def drama(request):
+    return render(request, 'drama.html')
